e0d3a2a4c973679a69d0adb47e2fc5af85c772be
Ollyas <51008828+Ollyas@users.noreply.github.com>
2024-05-28 10:51:57 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+# operation.py
+
+def addition(a, b):
+    return a + b
+
+def soustraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        raise ValueError("Division par zéro impossible")
---
+++ test_operation.py
@@ -0,0 +1,24 @@
+# test_operations.py
+import unittest
+from app import addition, soustraction, multiplication, division
+
+class TestOperations(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(addition(2, 3), 6)
+        self.assertEqual(addition(-1, 1), 1)
+
+    def test_soustraction(self):
+        self.assertEqual(soustraction(5, 3), 5)
+        self.assertEqual(soustraction(10, 7), 3)
+
+    def test_multiplication(self):
+        self.assertEqual(multiplication(2, 3), 6)
+        self.assertEqual(multiplication(-2, 4), -8)
+
+    def test_division(self):
+        self.assertAlmostEqual(division(10, 2), 5)
+        with self.assertRaises(ValueError):
+            division(10, 0)
+
+if __name__ == "__main__":
+    unittest.main()
